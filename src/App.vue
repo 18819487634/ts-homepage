1986cd71c23b0e5f1fab3fb25eef5735a0ab781f
zhongpengfei <zhongpengfei7634@dingtail.com>
2020-11-17 15:05:47 +0800

    fixed: 修改ts报错

--- src/App.vue
+++ src/App.vue
@@ -8,6 +8,13 @@
   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from 'vue'
+export default defineComponent({
+  name: 'App'
+})
+</script>
+
 <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
